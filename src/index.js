c69ebacce49fc467261e64bb2fac8fbc9dbd0545
ranggadwias <ranggadwi783@gmail.com>
2025-06-09 16:42:16 +0700

    fix: handle root path GET request

--- src/index.js
+++ src/index.js
@@ -9,11 +9,15 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials: true
 }));
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FinMate Backend is running");
+});
 
 app.use('/api/auth', authRouter);
 
